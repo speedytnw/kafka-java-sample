bdba8960c1c20b43f63177d515ed47793fc012cd
tintnaingwin <speedytnw@gmail.com>
2023-10-25 23:57:09 +0800

    prime-topic-producer

--- simple-producer/src/main/java/io/arete/kafka_sample/Main.java
+++ simple-producer/src/main/java/io/arete/kafka_sample/Main.java
@@ -1,8 +1,32 @@
 package io.arete.kafka_sample;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers","localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+        int count = 0;
+        for(int i=2; i< 100; i++) {
+            boolean isPrime = true;
+            for(int j=i-1; j>1; j--) {
+                if (i%j == 0) {
+                    isPrime =  false;
+                    break;
+                }
+            }
+            if (isPrime)
+                producer.send(new ProducerRecord<>("prime-topic","prime_" + ++count, String.valueOf(i)));
+        }
+        producer.close();
     }
 }
